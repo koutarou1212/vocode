f92bb8fe9a50d1682e236b21c250cdd5f2444f32
programming7777 <143686660+programming7777@users.noreply.github.com>
2024-06-06 23:33:34 +0000

    test code

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-
+N, M = map(int, input().split())
+A = map(int, input().split())
+X = [list(map(int, input().split())) for _ in range(N)]
+for I in map(list, zip(A, *X)):
+    if I[0] >= sum(I[1:]):
+        print("No")
+        exit()
+print("Yes")
